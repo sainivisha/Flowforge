62c20935352b8f54394eb49c2e63f49a735f7145
sainvishaa <sainivishaa@gmail.com>
2026-03-18 15:48:37 +0530

    feat: integrate Sentry for error tracking and monitoring
    - Added Sentry SDK to the project dependencies.
    - Configured Sentry for edge and server environments with appropriate DSN and integrations.
    - Implemented a sample API route to demonstrate error tracking with Sentry.
    - Created a global error handler to capture exceptions and report them to Sentry.
    - Developed a frontend page to trigger and test error reporting to Sentry.
    - Enhanced Inngest functions with Sentry logging for better observability.
    - Set up instrumentation for client-side error tracking and router transitions.
    - Updated TRPC router to utilize base procedures for AI execution.

---
+++ sentry.server.config.ts
@@ -0,0 +1,27 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://4f1d1fba8077c803b5542800564cf09c@o4511059627802624.ingest.us.sentry.io/4511059663192064",
+
+  integrations: [
+    Sentry.vercelAIIntegration({
+      recordInputs: true,
+      recordOutputs: true,
+    }),
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  sendDefaultPii: true,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/db";
 import { inngest } from "./client";
+import * as Sentry from "@sentry/nextjs";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { generateText } from "ai";
 
@@ -10,13 +11,24 @@ export const execute = inngest.createFunction(
   { event: "execute/ai" },
   async ({ event, step }) => {
     await step.sleep("pretend","5s");
+    
+    Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' })
+    console.warn("Something is missing");
+    console.error("This is an error i want to track");
+
+
     const { steps } = await step.ai.wrap(
      "gemini generate text",
      generateText,
       {
-       model: google("gemini-2.5-flash"),
-       system: "You are a helpful assistant",
-       prompt: "What is 2 + 2?",
+        model: google("gemini-2.5-flash"),
+        system: "You are a helpful assistant",
+        prompt: "What is 2 + 2?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       } 
     );
     return steps;
